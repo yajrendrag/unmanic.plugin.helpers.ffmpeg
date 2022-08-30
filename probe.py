6fabe97d3addc5bd45c019ab1b8724b3fd4fd3fb
Josh.5 <jsunnex@gmail.com>
2022-08-31 10:27:27 +1200

    Allow init_probe to be used from files other than just video mimetype

--- probe.py
+++ probe.py
@@ -150,15 +150,16 @@ class Probe(object):
         return True
 
     @staticmethod
-    def init_probe(data, logger):
+    def init_probe(data, logger, allowed_mimetypes=None):
         """
         Fetch the Probe object given a plugin's data object
 
         :param data:
         :param logger:
+        :param allowed_mimetypes:
         :return:
         """
-        probe = Probe(logger, allowed_mimetypes=['video'])
+        probe = Probe(logger, allowed_mimetypes=allowed_mimetypes)
         if 'ffprobe' in data.get('shared_info', {}):
             if not probe.set_probe(data.get('shared_info', {}).get('ffprobe')):
                 # Failed to set ffprobe from shared info.
